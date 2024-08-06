eb372c97407bc47ad45b48e311a571f891f544e4
liza kh <liza7976@gmail.com>
2024-08-06 21:28:52 +0300

    Add project files.

---
+++ WebApplication12/Controllers/GitHubController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Results;
+
+public class GitHubController : ApiController
+{
+    [HttpPost]
+    [Route("api/github/bookmark")]
+    public IHttpActionResult BookmarkRepository([FromBody] Repository repository)
+    {
+        var sessionBookmarks = HttpContext.Current.Session["Bookmarks"] as List<Repository>;
+        if (sessionBookmarks == null)
+        {
+            sessionBookmarks = new List<Repository>();
+        }
+        sessionBookmarks.Add(repository);
+        HttpContext.Current.Session["Bookmarks"] = sessionBookmarks;
+        return Ok();
+    }
+
+    [HttpGet]
+    [Route("api/github/bookmarks")]
+    public JsonResult<List<Repository>> GetBookmarks()
+    {
+        var sessionBookmarks = HttpContext.Current.Session["Bookmarks"] as List<Repository>;
+        if (sessionBookmarks == null)
+        {
+            sessionBookmarks = new List<Repository>();
+        }
+        return Json(sessionBookmarks);
+    }
+
+    [HttpPost]
+    [Route("api/github/sendemail")]
+    public IHttpActionResult SendEmail([FromBody] EmailRequest request)
+    {
+        try
+        {
+            var message = new MailMessage();
+            message.To.Add(request.EmailAddress);
+            message.Subject = "GitHub Repository Details";
+            message.Body = $"Repository Name: {request.Repository.name}\nURL: {request.Repository.html_url}";
+            var smtpClient = new SmtpClient();
+            smtpClient.Send(message);
+            return Ok();
+        }
+        catch (SmtpException ex)
+        {
+            return InternalServerError(ex);
+        }
+    }
+}
+
+public class Repository
+{
+    public string name { get; set; }
+    public string html_url { get; set; }
+    public Owner owner { get; set; }
+}
+
+public class Owner
+{
+    public string avatar_url { get; set; }
+}
+
+public class EmailRequest
+{
+    public string EmailAddress { get; set; }
+    public Repository Repository { get; set; }
+}
---
+++ WebApplication12/Scripts/app/app.js
@@ -0,0 +1,53 @@
+﻿var app = angular.module('githubApp', []);
+
+app.controller('GitHubController', function ($scope, $http) {
+    $scope.searchTerm = '';
+    $scope.repositories = [];
+    $scope.showEmailPopup = false;
+    $scope.emailAddress = '';
+    $scope.selectedRepo = null;
+
+    $scope.search = function () {
+        $http.get('https://api.github.com/search/repositories?q=' + $scope.searchTerm)
+            .then(function (response) {
+                $scope.repositories = response.data.items;
+            }, function (error) {
+                console.error('Error fetching repositories:', error);
+            });
+    };
+
+    $scope.bookmark = function (repo) {
+        $http.post('/api/github/bookmark', repo)
+            .then(function (response) {
+                alert('Repository bookmarked!');
+            }, function (error) {
+                console.error('Error bookmarking repository:', error);
+            });
+    };
+
+    $scope.openEmailPopup = function (repo) {
+        $scope.selectedRepo = repo;
+        $scope.showEmailPopup = true;
+    };
+
+    $scope.closeEmailPopup = function () {
+        $scope.showEmailPopup = false;
+        $scope.emailAddress = '';
+        $scope.selectedRepo = null;
+    };
+
+    $scope.sendEmail = function () {
+        var emailRequest = {
+            EmailAddress: $scope.emailAddress,
+            Repository: $scope.selectedRepo
+        };
+
+        $http.post('/api/github/sendemail', emailRequest)
+            .then(function (response) {
+                alert('Email sent!');
+                $scope.closeEmailPopup();
+            }, function (error) {
+                console.error('Error sending email:', error);
+            });
+    };
+});
